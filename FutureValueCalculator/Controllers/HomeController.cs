0c776a79adea679227a18917c591aec084c12f4f
RyanTu-SoftwareDeveloper <ryantu89.work@gmail.com>
2024-04-01 13:34:04 -0700

    Add project files.

---
+++ FutureValueCalculator/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FutureValueCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Ryan";
+            ViewBag.FV = 99999.99;
+            return View();
+        }
+    }
+}
